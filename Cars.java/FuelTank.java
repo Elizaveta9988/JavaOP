762d185ac4536d176206aa812c0691e1bf418aeb
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2023-07-15 23:40:41 +0300

    first commit

---
+++ Cars.java/FuelTank.java
@@ -0,0 +1,20 @@
+package Cars.java;
+
+public class FuelTank  extends CarPart{
+    private double gas;
+
+    public FuelTank() {
+        super("Fuel Tank", 100);
+        this.gas = 100;
+    }
+
+    public double getFuel() {
+        return this.gas;
+    }
+
+    public void setGas(double miles) {
+        this.gas -= miles * 0.075;
+    }
+
+}
+
